e139c5eab051f5af1f023a508ebcb0eab9f84032
Erzhan-dev <77715232+Erzhan-dev@users.noreply.github.com>
2021-09-28 20:23:23 +0600

    third commit

--- UsersRESTAPI/Models/UserModel.cs
+++ UsersRESTAPI/Models/UserModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace UsersRESTAPI.Models
 {
     public class UserModel
@@ -17,6 +19,7 @@ namespace UsersRESTAPI.Models
         [Required]
         public string BirthDate { get; set; }
         [Required]
+        [JsonIgnore]
         public string password { get; set; }
     }
 
